0b83a3053c63c268676acb3c049a1cd54b651300
Faith-arc <90138065+Faith-arc@users.noreply.github.com>
2021-09-08 06:00:15 +0700

    Update inc_header.php

--- inc_header.php
+++ inc_header.php
@@ -9,20 +9,20 @@ include_once("inc/inc_fungsi.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Programming di RumahRafif.</title>
+    <title>Historiaberita.</title>
     <link rel="stylesheet" href="<?php echo url_dasar() ?>/css/style.css">
 </head>
 
 <body>
     <nav>
         <div class="wrapper">
-            <div class="logo"><a href='<?php echo url_dasar() ?>'>RumahRafif.</a></div>
+            <div class="logo"><a href='<?php echo url_dasar() ?>'>Historiaberita.</a></div>
             <div class="menu">
                 <ul>
                     <li><a href="<?php echo url_dasar() ?>#home">Home</a></li>
-                    <li><a href="<?php echo url_dasar() ?>#courses">Courses</a></li>
-                    <li><a href="<?php echo url_dasar() ?>#tutors">Tutors</a></li>
-                    <li><a href="<?php echo url_dasar() ?>#partners">Partners</a></li>
+                    <li><a href="<?php echo url_dasar() ?>#aboutus">AboutUs</a></li>
+                    <li><a href="<?php echo url_dasar() ?>#article">Article</a></li>
+                    <li><a href="<?php echo url_dasar() ?>#gallery">Gallery</a></li>
                     <li><a href="<?php echo url_dasar() ?>#contact">Contact</a></li>
                     <li>
                         <?php if (isset($_SESSION['members_nama_lengkap'])) {
